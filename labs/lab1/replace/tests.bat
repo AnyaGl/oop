c6fcf60d096f0625323a588ded69e986a48dde7c
AnyaGl <gladysheva_2000@inbox.ru>
2020-02-15 19:11:55 +0300

    Добавлены тесты

--- labs/lab1/replace/tests.bat
+++ labs/lab1/replace/tests.bat
@@ -27,21 +27,36 @@
  fc test-data\output4.txt "%TEMP%\output.txt" > nul || goto err
  echo Test 4 passed
 
+ REM Замена подстроки, находящейся в начале, середине и конце файла
+ %Program% test-data\input5.txt "%TEMP%\output.txt" "123" "*" || goto err
+ fc test-data\output5.txt "%TEMP%\output.txt" > nul || goto err
+ echo Test 5 passed
+ 
+ REM Замена строки, из которой состоит файл
+ %Program% test-data\input6.txt "%TEMP%\output.txt" "replace all" "*" || goto err
+ fc test-data\output6.txt "%TEMP%\output.txt" > nul || goto err
+ echo Test 6 passed
+ 
+ REM Замена подстроки, которой нет в файле
+ %Program% test-data\input7.txt "%TEMP%\output.txt" "123" "*" || goto err
+ fc test-data\output7.txt "%TEMP%\output.txt" > nul || goto err
+ echo Test 7 passed
+ 
  REM Копирование из несуществующего файла
  %Program% MissingFile.txt "%TEMP%\output.txt" "_" "*" && goto err
- echo Test 5 passed
+ echo Test 8 passed
 
  REM Неверно заданные аргументы командной строки: нет одного из файлов
  %Program% test-data\input1.txt "_" "*" && goto err
- echo Test 6 passed
+ echo Test 9 passed
 
  REM Неверно заданные аргументы командной строки: нет одной из строк
  %Program% test-data\input1.txt "%TEMP%\output.txt" "*" && goto err
- echo Test 7 passed
+ echo Test 10 passed
 
  REM Неверно заданные аргументы командной строки: нет ни одного аргумента
  %Program% && goto err
- echo Test 8 passed
+ echo Test 11 passed
 
  echo All tests passed successfully
  exit /B 0
