c9ab28bf4900e15fee3d461ea2a466dc8cff1971
AnyaGl <gladysheva_2000@inbox.ru>
2020-02-15 13:16:08 +0300

    Добавлено копирование входного файла в выходной с заменой подстроки

--- labs/lab1/replace/replace.cpp
+++ labs/lab1/replace/replace.cpp
@@ -15,24 +15,44 @@ int main(int argc, char *argv[]) {
 
   ifstream inFile;
   inFile.open(argv[1]);
-
-  if (!inFile) {
-    cerr << argv[1] << " could not be opened!" << endl;
-    exit(1);
+  if (!inFile.is_open()) {
+    cout << argv[1] << " could not be opened!\n";
+    return 1;
   }
 
   ofstream outFile;
   outFile.open(argv[2]);
+  if (!outFile.is_open()) {
+      cout << argv[2] << " could not be opened!\n";
+      return 1;
+  }
 
   string searchStr = argv[3];
   string replaceStr = argv[4];
 
-  string currStr;
-  int index;
+  string currStr, resultStr;
+  int index, foundPos;
   int changeInd = replaceStr.length() - searchStr.length();
 
   while (getline(inFile, currStr)) {
-    cout << currStr;
+    index = 0;
+    resultStr = "";
+
+    while (index < currStr.length()) {
+        foundPos = currStr.find(searchStr, index);
+        
+        resultStr.append(currStr, index, foundPos - index);
+
+        if (foundPos != string::npos) {
+            resultStr.append(replaceStr);
+            index = foundPos + searchStr.length();
+        }
+        else {
+            index = currStr.length();
+        }
+        
+    }
+    outFile << resultStr << '\n';
   }
 
   return 0;
