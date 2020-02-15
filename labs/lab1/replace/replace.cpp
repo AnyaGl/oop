694d8bee2583c802edeab71dd79ed6735431ce1b
AnyaGl <gladysheva_2000@inbox.ru>
2020-02-15 14:00:14 +0300

    Парсинг аргументов командной строки вынесен в отдельную функцию

--- labs/lab1/replace/replace.cpp
+++ labs/lab1/replace/replace.cpp
@@ -1,56 +1,74 @@
 ﻿#include <fstream>
 #include <iostream>
+#include <optional>
 #include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+struct Args {
+  string inputFileName;
+  string outputFileName;
+  string searchString;
+  string replaceString;
+};
 
+optional<Args> ParseArgs(int argc, char *argv[]) {
   if (argc != 5) {
     cout << "Error: number of arguments\n"
          << "Use: replace.exe <input file> <output file> <search string> "
             "<replace string>\n";
+    return nullopt;
+  }
+  Args args;
+  args.inputFileName = argv[1];
+  args.outputFileName = argv[2];
+  args.searchString = argv[3];
+  args.replaceString = argv[4];
+  return args;
+}
+
+int main(int argc, char *argv[]) {
+
+  auto args = ParseArgs(argc, argv);
+  if (!args) {
     return 1;
   }
 
   ifstream inFile;
-  inFile.open(argv[1]);
+  inFile.open(args->inputFileName);
   if (!inFile.is_open()) {
-    cout << argv[1] << " could not be opened!\n";
+    cout << args->inputFileName << " could not be opened!\n";
     return 1;
   }
 
   ofstream outFile;
-  outFile.open(argv[2]);
+  outFile.open(args->outputFileName);
   if (!outFile.is_open()) {
-      cout << argv[2] << " could not be opened!\n";
-      return 1;
+    cout << args->outputFileName << " could not be opened!\n";
+    return 1;
   }
 
-  string searchStr = argv[3];
-  string replaceStr = argv[4];
+  string searchStr = args->searchString;
+  string replaceStr = args->replaceString;
 
   string currStr, resultStr;
   int index, foundPos;
-  int changeInd = replaceStr.length() - searchStr.length();
 
   while (getline(inFile, currStr)) {
     index = 0;
     resultStr = "";
 
     while (index < currStr.length()) {
-        foundPos = currStr.find(searchStr, index);
-        
-        resultStr.append(currStr, index, foundPos - index);
+      foundPos = currStr.find(searchStr, index);
 
-        if (foundPos != string::npos) {
-            resultStr.append(replaceStr);
-            index = foundPos + searchStr.length();
-        }
-        else {
-            index = currStr.length();
-        }
-        
+      resultStr.append(currStr, index, foundPos - index);
+
+      if (foundPos != string::npos) {
+        resultStr.append(replaceStr);
+        index = foundPos + searchStr.length();
+      } else {
+        index = currStr.length();
+      }
     }
     outFile << resultStr << '\n';
   }
