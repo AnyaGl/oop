1d57af89969172a6c2373cc1e062c9e2ab0fed32
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-07 15:40:35 +0300

    Добавлена функция чтения массива

---
+++ labs/lab2/vector/vector.cpp
@@ -0,0 +1,20 @@
+﻿#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+void ReadArray(std::istream& input, std::vector<double>& array)
+{
+	std::copy(std::istream_iterator<double>(input), std::istream_iterator<double>(), std::back_inserter(array));
+}
+
+int main()
+{
+	std::vector<double> array;
+	ReadArray(std::cin, array);
+
+	copy(array.begin(), array.end(), std::ostream_iterator<double>(std::cout, ", "));
+
+	return 0;
+}
