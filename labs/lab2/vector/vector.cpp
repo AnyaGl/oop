a4018bbbc0ef524d3994e2e364c6c6a6a65a57b0
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-07 20:44:01 +0300

    Доработана программа vector

--- labs/lab2/vector/vector.cpp
+++ labs/lab2/vector/vector.cpp
@@ -1,20 +1,44 @@
 ﻿#include <algorithm>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <vector>
 #include <iterator>
+#include <vector>
 
-void ReadArray(std::istream& input, std::vector<double>& array)
+void ProcessVector(std::vector<double>& numbers)
 {
-	std::copy(std::istream_iterator<double>(input), std::istream_iterator<double>(), std::back_inserter(array));
+	double sumNegativeNum = 0;
+	for (double& number : numbers)
+	{
+		if (number < 0)
+		{
+			sumNegativeNum += number;
+		}
+	}
+
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		if (i % 2 == 0)
+		{
+			numbers[i] *= 2;
+		}
+		else
+		{
+			numbers[i] -= sumNegativeNum;
+		}
+	}
 }
 
 int main()
 {
-	std::vector<double> array;
-	ReadArray(std::cin, array);
+	std::vector<double> numbers(std::istream_iterator<double>(std::cin), (std::istream_iterator<double>()));
 
-	copy(array.begin(), array.end(), std::ostream_iterator<double>(std::cout, ", "));
+	ProcessVector(numbers);
+	
+	std::sort(numbers.begin(), numbers.end());
+
+	std::cout << std::fixed << std::setprecision(3);
+	std::copy(numbers.begin(), numbers.end(), std::ostream_iterator<double>(std::cout, " "));
 
 	return 0;
 }
