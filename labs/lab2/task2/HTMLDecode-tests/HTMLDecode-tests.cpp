8149973b9044de7386a2a7c6202a0bcfa964ff76
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-08 23:42:43 +0300

    Добавлены тесты, проверяющие работу функции HtmlDecode()

---
+++ labs/lab2/task2/HTMLDecode-tests/HTMLDecode-tests.cpp
@@ -0,0 +1,16 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../HTMLDecode/Decoder.h"
+#include "catch2/catch.hpp"
+
+TEST_CASE("Сheck the result of ProcessVector()")
+{
+	std::string html = "Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s";
+	std::string decodedHtml = "Cat <says> \"Meow\". M&M's";
+
+	CHECK(HtmlDecode(html) == decodedHtml);
+
+	html = "&&&amp;;;";
+	decodedHtml = "&&&;;";
+
+	CHECK(HtmlDecode(html) == decodedHtml);
+}
