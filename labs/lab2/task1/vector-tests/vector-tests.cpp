ff8852b51bd96cefa6fe68c6795c7fd93a88522c
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-08 18:29:28 +0300

    Добавлены тесты

--- labs/lab2/task1/vector-tests/vector-tests.cpp
+++ labs/lab2/task1/vector-tests/vector-tests.cpp
@@ -2,11 +2,25 @@
 #include "catch2/catch.hpp"
 #include "../vector/ProcessVector.h"
 
-TEST_CASE("...")
+TEST_CASE("Сheck the result of ProcessVector()")
 {
-	std::vector<double> numbers = { 2, 1, -4};
+	std::vector<double> numbers;
 	ProcessVector(numbers);
-	std::vector<double> required = { 4.000, 5.000, -8.000};
+	std::vector<double> required;
+
+	CHECK(numbers == required);
+
+
+	numbers = { 2, 1, -4};
+	ProcessVector(numbers);
+	required = { 4, 5, -8};
+
+	CHECK(numbers == required);
+
+
+	numbers = { 1.2333, -2.45, -1, -4, -2.55};
+	ProcessVector(numbers);
+	required = { 2.4666, 7.55, -2, 6, -5.1};
 
 	CHECK(numbers == required);
 
