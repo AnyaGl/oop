268647160e273852ca64782e0d95484a0a46766d
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-10 21:25:45 +0300

    Доработаны тесты

--- labs/lab2/task2/HTMLDecode-tests/HTMLDecode-tests.cpp
+++ labs/lab2/task2/HTMLDecode-tests/HTMLDecode-tests.cpp
@@ -2,7 +2,7 @@
 #include "../HTMLDecode/Decoder.h"
 #include "catch2/catch.hpp"
 
-TEST_CASE("Сheck the result of ProcessVector()")
+TEST_CASE("Сheck the result of HtmlDecode()")
 {
 	std::string html = "Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s";
 	std::string decodedHtml = "Cat <says> \"Meow\". M&M's";
