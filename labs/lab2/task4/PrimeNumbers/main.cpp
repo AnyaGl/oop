aae613f81c211749df792e795cfbe5bb2c75cc58
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-22 15:57:42 +0300

    Разработана программа, выводящая простые числа

--- labs/lab2/task4/PrimeNumbers/main.cpp
+++ labs/lab2/task4/PrimeNumbers/main.cpp
@@ -1,7 +1,34 @@
 #include "PrimeNumbers.h"
 #include <iostream>
+const int MAX_VALUE = 100000000;
 
-int main()
+void PrintNumbers(std::set<int> numbers)
 {
+	for (auto it = numbers.begin(); it != numbers.end(); it++)
+	{
+		std::cout << *(it) << '\n';
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		std::cout
+			<< "Invalid number of arguments\n"
+			<< "Use: PrimeNumbers.exe <upper bound>\n";
+		return 1;
+	}
+	int upperBound = atoi(argv[1]);
+
+	if (upperBound > MAX_VALUE)
+	{
+		std::cout
+			<< "Use upper bound <= 100'000'000\n";
+		return 1;
+	}
+
+	std::set<int> primeNumbers = GeneratePrimeNumbersSet(upperBound);
+	PrintNumbers(primeNumbers);
 	return 0;
 }
