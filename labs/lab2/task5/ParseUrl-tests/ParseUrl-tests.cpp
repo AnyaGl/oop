5ba1c698bbe3ad5100d469436dc9d9b25b65bb6a
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-22 22:20:17 +0300

    Добавлена функция ParseURL и тесты к ней

---
+++ labs/lab2/task5/ParseUrl-tests/ParseUrl-tests.cpp
@@ -0,0 +1,80 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../ParseUrl/ParseUrl.h"
+#include "catch2/catch.hpp"
+
+using namespace std::literals;
+
+Protocol protocol;
+int port;
+std::string host;
+std::string document;
+
+Protocol reqProtocol;
+int reqPort;
+std::string reqHost;
+std::string reqDocument;
+
+TEST_CASE("Parse an empty string must be false")
+{
+	REQUIRE(!ParseURL("", protocol, port, host, document));
+}
+
+TEST_CASE("Parse an url must return protocol, port, host and document")
+{
+	reqProtocol = Protocol::HTTPS;
+	reqPort = 123;
+	reqHost = "www.mysite";
+	reqDocument = "docs/document1.html";
+
+	ParseURL("https://www.mysite:123/docs/document1.html"s, protocol, port, host, document);
+	REQUIRE(protocol == reqProtocol);
+	REQUIRE(port == reqPort);
+	REQUIRE(host == reqHost);
+	REQUIRE(document == reqDocument);
+}
+
+TEST_CASE("Parse an incorrect url must be false")
+{
+	REQUIRE(!ParseURL("http:/"s, protocol, port, host, document));
+	REQUIRE(!ParseURL("http:/me.com"s, protocol, port, host, document));
+}
+
+TEST_CASE("Parse an url with incorrect protocol must be false")
+{
+	REQUIRE(!ParseURL("htp://my.com"s, protocol, port, host, document));
+}
+
+TEST_CASE("Parse an url with incorrect host must be false")
+{
+	REQUIRE(!ParseURL("http://my.com%"s, protocol, port, host, document));
+}
+
+TEST_CASE("Parse an url without port must return default port")
+{
+	reqPort = 80;
+	ParseURL("http://my.com"s, protocol, port, host, document);
+	REQUIRE(port == reqPort);
+
+	reqPort = 443;
+	ParseURL("https://my.com"s, protocol, port, host, document);
+	REQUIRE(port == reqPort);
+
+	reqPort = 21;
+	ParseURL("ftp://my.com"s, protocol, port, host, document);
+	REQUIRE(port == reqPort);
+}
+
+TEST_CASE("Parse an url with incorrect port must be false")
+{
+	REQUIRE(!ParseURL("http://my.com:-1"s, protocol, port, host, document));
+	REQUIRE(!ParseURL("https://my.com:0"s, protocol, port, host, document));
+	REQUIRE(!ParseURL("ftp://my.com:65536"s, protocol, port, host, document));
+}
+
+TEST_CASE("Parse an url without document must return empty document")
+{
+	reqDocument = "";
+
+	ParseURL("https://www.mysite:123"s, protocol, port, host, document);
+	REQUIRE(document == reqDocument);
+}
---
+++ labs/lab2/task5/ParseUrl/ParseUrl.cpp
@@ -0,0 +1,77 @@
+﻿#include "ParseUrl.h"
+#include <algorithm>
+#include <map>
+#include <optional>
+#include <regex>
+
+std::map<Protocol, int> defaultPort = {
+	{ Protocol::HTTP, 80 },
+	{ Protocol::HTTPS, 443 },
+	{ Protocol::FTP, 21 },
+};
+
+std::optional<Protocol> GetProtocol(std::string protocol)
+{
+	std::transform(protocol.begin(), protocol.begin(), protocol.end(), tolower);
+	if (protocol == "http")
+	{
+		return Protocol::HTTP;
+	}
+	if (protocol == "https")
+	{
+		return Protocol::HTTPS;
+	}
+	if (protocol == "ftp")
+	{
+		return Protocol::FTP;
+	}
+	return std::nullopt;
+}
+
+std::optional<int> GetPort(std::string port, Protocol const& protocol)
+{
+	if (port.empty())
+	{
+		return defaultPort[protocol];
+	}
+
+	int resultPort;
+	resultPort = atoi(port.c_str());
+	if (resultPort >= 1 && resultPort <= 65535)
+	{
+		return resultPort;
+	}
+
+	return std::nullopt;
+}
+
+bool ParseURL(std::string const& url, Protocol& protocol, int& port, std::string& host, std::string& document)
+{
+	std::smatch matches;
+	std::regex rx("^(.*)://([-.[:alnum:]]+)(:([[:digit:]]+))?(/(.*))?$");
+	std::regex_search(url, matches, rx);
+
+	if (matches.empty())
+	{
+		return false;
+	}
+
+	std::optional<Protocol> resultProtocol = GetProtocol(matches[1]);
+	if (!resultProtocol)
+	{
+		return false;
+	}
+	protocol = resultProtocol.value();
+
+	std::optional<int> resultPort = GetPort(matches[4], protocol);
+	if (!resultPort)
+	{
+		return false;
+	}
+	port = resultPort.value();
+
+	host = matches[2];
+	document = matches[6];
+
+	return true;
+}
