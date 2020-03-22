f4e73d91b0991113f90454032d11b8ceeb4c3168
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-22 22:27:35 +0300

    Разработана программа, распознающая URL-строки

--- labs/lab2/task5/ParseUrl/main.cpp
+++ labs/lab2/task5/ParseUrl/main.cpp
@@ -1,7 +1,32 @@
 #include "ParseUrl.h"
 #include <iostream>
 
+void PrintUrlInfo(std::string const& host, int port, std::string const& document)
+{
+	std::cout << "HOST: " << host << '\n';
+	std::cout << "PORT: " << port << '\n';
+	std::cout << "DOC: " << document << '\n';
+}
+
 int main()
 {
+	std::string url;
+	Protocol protocol;
+	int port;
+	std::string host;
+	std::string document;
+
+	while (getline(std::cin, url))
+	{
+		if (ParseURL(url, protocol, port, host, document))
+		{
+			PrintUrlInfo(host, port, document);
+		}
+		else
+		{
+			std::cout << "Incorrect URL\n";
+		}
+	}
+
 	return 0;
 }
