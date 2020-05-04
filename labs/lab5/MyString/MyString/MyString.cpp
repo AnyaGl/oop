793b4845f1c5dc303cc9f71262cf488d57c22133
AnyaGl <gladysheva_2000@inbox.ru>
2020-05-04 17:39:41 +0300

    Создан класс CMyString и разработаны тесты

---
+++ labs/lab5/MyString/MyString/MyString.cpp
@@ -0,0 +1,135 @@
+﻿#include "MyString.h"
+
+CMyString::CMyString()
+	: m_length(0)
+{
+	m_pString = new char[m_length + 1];
+	m_pString[m_length] = '\0';
+}
+
+CMyString::CMyString(const char* pString)
+	: CMyString(pString, strlen(pString))
+{
+}
+
+CMyString::CMyString(const char* pString, size_t length)
+	: m_length(length)
+{
+	m_pString = new char[m_length + 1];
+	memcpy(m_pString, pString, m_length + 1);
+	m_pString[m_length] = '\0';
+}
+
+CMyString::CMyString(CMyString const& other)
+	: CMyString(other.m_pString, other.m_length)
+{
+}
+
+CMyString::CMyString(CMyString&& other)
+	: m_pString(other.m_pString)
+	, m_length(other.m_length)
+{
+	other.m_pString = nullptr;
+	other.m_length = 0;
+}
+
+CMyString::CMyString(std::string const& stlString)
+	: CMyString(stlString.c_str(), stlString.size())
+{
+}
+
+CMyString::~CMyString()
+{
+	delete[] m_pString;
+}
+
+size_t CMyString::GetLength() const
+{
+	return size_t();
+}
+
+const char* CMyString::GetStringData() const
+{
+	return nullptr;
+}
+
+CMyString CMyString::SubString(size_t start, size_t length) const
+{
+	return CMyString();
+}
+
+void CMyString::Clear()
+{
+}
+
+CMyString& CMyString::operator=(const CMyString& other)
+{
+	return *this;
+}
+
+CMyString& CMyString::operator=(CMyString&& other)
+{
+	return *this;
+}
+
+
+
+CMyString& CMyString::operator+=(const CMyString& other)
+{
+	return *this;
+}
+
+const char& CMyString::operator[](size_t index) const
+{
+	return m_pString[0];
+}
+
+char& CMyString::operator[](size_t index)
+{
+	return m_pString[0];
+}
+
+CMyString const operator+(const CMyString& str1, const CMyString& str2)
+{
+	return nullptr;
+}
+
+bool operator==(const CMyString& str1, const CMyString& str2)
+{
+	return false;
+}
+
+bool operator!=(const CMyString& str1, const CMyString& str2)
+{
+	return false;
+}
+
+bool operator<(const CMyString& str1, const CMyString& str2)
+{
+	return false;
+}
+
+bool operator>(const CMyString& str1, const CMyString& str2)
+{
+	return false;
+}
+
+bool operator<=(const CMyString& str1, const CMyString& str2)
+{
+	return false;
+}
+
+bool operator>=(const CMyString& str1, const CMyString& str2)
+{
+	return false;
+}
+
+std::ostream& operator<<(std::ostream& stream, const CMyString& rational)
+{
+	return stream;
+}
+
+std::istream& operator>>(std::istream& stream, CMyString& rational)
+{
+	return stream;
+}
---
+++ labs/lab5/MyString/MyStringTests/MyStringTests.cpp
@@ -0,0 +1,134 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+#include "../MyString/MyString.h"
+#include <sstream>
+
+using namespace std::literals;
+
+TEST_CASE("Конструктор по умолчанию формирует строку нулевой длины")
+{
+	CMyString str;
+	CHECK(str.GetLength() == 0);
+	CHECK(str.GetStringData() == "\0");
+}
+
+TEST_CASE("Конструктор, принимающий строку stl, инициализирует строку данными этой строки")
+{
+	CMyString str("asdbcd"s);
+	CHECK(str.GetLength() == 6);
+	CHECK(str.GetStringData() == "asdbcd");
+}
+
+TEST_CASE("Конструктор, принимающий указатель на массив символов, инициализирует строку данными строки с завершающим нулевым символом")
+{
+	CMyString str("asdbcd");
+	CHECK(str.GetLength() == 6);
+	CHECK(str.GetStringData() == "asdbcd");
+}
+
+TEST_CASE("Конструктор, принимающий указатель на массив символов и длину, инициализирует строку данными из массива заданной длины")
+{
+	CMyString str("asd\0bcd", 6);
+	CHECK(str.GetLength() == 6);
+	CHECK(str.GetStringData() == "asd\0bc");
+}
+
+TEST_CASE("SubString() должен возвращать подстроку с заданной позиции длиной не больше length символов")
+{
+	CMyString str("123\0qwerty", 10);
+	CHECK(str.SubString(1) == "123\0qwerty");
+	CHECK(str.SubString(4, 2) == "\0q");
+	CHECK(str.SubString(4, 10) == "\0qwerty");
+	CHECK_THROWS(str.SubString(10));
+}
+
+TEST_CASE("Clear() должен очищать строку (строка становится нулевой длины)")
+{
+	CMyString str("123\0qwerty", 10);
+	str.Clear();
+	CHECK(str.GetLength() == 0);
+	CHECK(str.GetStringData() == "\0");
+}
+
+TEST_CASE("= должен выполнять конкатенацию двух строк")
+{
+	CMyString resString = "123\0" + CMyString("qwerty");
+	CHECK(resString.GetLength() == 10);
+	CHECK(resString.GetStringData() == "123\0qwerty");
+
+	resString = "123\0"s + CMyString("qwerty");
+	CHECK(resString.GetLength() == 11);
+	CHECK(resString.GetStringData() == "123\\0qwerty");
+
+	resString = CMyString("123\0") + CMyString("qwerty");
+	CHECK(resString.GetLength() == 11);
+	CHECK(resString.GetStringData() == "123\0qwerty");
+}
+
+TEST_CASE("+= должен выполнять конкатенацию двух строк с присваиванием")
+{
+	CMyString resString("123\0");
+	resString += CMyString("qwerty");
+	CHECK(resString.GetLength() == 10);
+	CHECK(resString.GetStringData() == "123\0qwerty");
+}
+
+TEST_CASE("== должен проверить две строки на равенство")
+{
+	CMyString str1("123\0");
+	CMyString str2("123");
+	CHECK(!(str1 == str2));
+	str2 += "\0";
+	CHECK(str1 == str2);
+}
+
+TEST_CASE("!= должен проверить две строки на неравенство")
+{
+	CMyString str1("123\0");
+	CMyString str2("123");
+	CHECK(str1 != str2);
+	str2 += "\0";
+	CHECK(!(str1 != str2));
+}
+
+TEST_CASE("<, >, <=, >= должны сравнивать две строки")
+{
+	CMyString str1("1235");
+	CMyString str2("1234");
+	CHECK(!(str1 < str2));
+	CHECK(str1 > str2);
+	CHECK(!(str1 <= str2));
+	CHECK(str1 >= str2);
+
+	str2 += "\0";
+	CHECK(str1 < str2);
+	CHECK(!(str1 > str2));
+	CHECK(str1 <= str2);
+	CHECK(!(str1 >= str2));
+}
+
+TEST_CASE("[] должен осуществлять индексированный доступ к символам строки по целочисленному индексу для чтения или записи")
+{
+	CMyString str("123\0q");
+	CHECK(str[3] == '\0');
+	str[3] = '4';
+	CHECK(str[3] == 4);
+}
+
+TEST_CASE(">> должен считывать из входного потока")
+{
+	CMyString str;
+	std::stringstream ss;
+	ss << "123\0q";
+	ss >> str;
+	CHECK(str.GetLength() == 5);
+	CHECK(str.GetStringData() == "123\0q");
+}
+
+TEST_CASE("<< должен записывать в выходной поток")
+{
+	CMyString str("123\0q", 5);
+	std::stringstream ss;
+	ss << str;
+	CHECK(ss.str() == "123\0q");
+}
