0252d9f11885541baddb56ccafc8a33cb43e61ee
AnyaGl <gladysheva_2000@inbox.ru>
2020-05-14 11:19:07 +0300

    Разработан класс CHttpUrl и написаны тесты

---
+++ labs/lab6/HttpUrl/HttpUrl/HttpUrl.cpp
@@ -0,0 +1,38 @@
+﻿#include "HttpUrl.h"
+
+CHttpUrl::CHttpUrl(std::string const& url)
+{
+}
+
+CHttpUrl::CHttpUrl(std::string const& domain, std::string const& document, Protocol protocol)
+{
+}
+
+CHttpUrl::CHttpUrl(std::string const& domain, std::string const& document, Protocol protocol, unsigned short port)
+{
+}
+
+std::string CHttpUrl::GetURL() const
+{
+	return std::string();
+}
+
+std::string CHttpUrl::GetDomain() const
+{
+	return std::string();
+}
+
+std::string CHttpUrl::GetDocument() const
+{
+	return std::string();
+}
+
+Protocol CHttpUrl::GetProtocol() const
+{
+	return Protocol();
+}
+
+unsigned short CHttpUrl::GetPort() const
+{
+	return 0;
+}
---
+++ labs/lab6/HttpUrl/HttpUrlTests/HttpUrlTests.cpp
@@ -0,0 +1,93 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../HttpUrl/HttpUrl.h"
+#include "catch2/catch.hpp"
+
+TEST_CASE("Constructor that takes URL must parse it")
+{
+	CHttpUrl url("https://www.mysite:123/docs/document1.html");
+	CHECK(url.GetDomain() == "www.mysite");
+	CHECK(url.GetDocument() == "/docs/document1.html");
+	CHECK(url.GetProtocol() == Protocol::HTTPS);
+	CHECK(url.GetPort() == 123);
+
+	SECTION("If failed, an exception must be thrown")
+	{
+		CHECK_THROWS(CHttpUrl("http:/my.com"));
+		CHECK_THROWS(CHttpUrl("httpk://my.com"));
+		CHECK_THROWS(CHttpUrl("http://my.com:65536"));
+	}
+}
+
+TEST_CASE("Constructor, that takes domain, document, [protocol], must initialize url")
+{
+	CHttpUrl url("www.mysite", "/doc", Protocol::HTTPS);
+	CHECK(url.GetDomain() == "www.mysite");
+	CHECK(url.GetDocument() == "/docs");
+	CHECK(url.GetProtocol() == Protocol::HTTPS);
+	CHECK(url.GetPort() == 80);
+
+	SECTION("Default protocol must be HTTP")
+	{
+		CHttpUrl url1("www.mysite", "/doc");
+		CHECK(url1.GetProtocol() == Protocol::HTTP);
+	}
+
+	SECTION("If document doesn't start with '/', then constructor must add '/'")
+	{
+		CHttpUrl url1("www.mysite", "doc");
+		CHECK(url1.GetDocument() == "/doc");
+
+		CHttpUrl url2("www.mysite", "");
+		CHECK(url2.GetDocument() == "/");
+	}
+
+	SECTION("If failed, an exception must be thrown")
+	{
+		CHECK_THROWS("", "/doc");
+	}
+}
+
+TEST_CASE("Constructor, that takes domain, document, protocol, port, must initialize url")
+{
+	CHttpUrl url("www.mysite", "/doc", Protocol::HTTPS, 123);
+	CHECK(url.GetDomain() == "www.mysite");
+	CHECK(url.GetDocument() == "/docs");
+	CHECK(url.GetProtocol() == Protocol::HTTPS);
+	CHECK(url.GetPort() == 123);
+
+	SECTION("If document doesn't start with '/', then constructor must add '/'")
+	{
+		CHttpUrl url1("www.mysite", "doc", Protocol::HTTPS, 123);
+		CHECK(url1.GetDocument() == "/doc");
+
+		CHttpUrl url2("www.mysite", "", Protocol::HTTPS, 123);
+		CHECK(url2.GetDocument() == "/");
+	}
+
+	SECTION("If failed, an exception must be thrown")
+	{
+		CHECK_THROWS("", "/doc", Protocol::HTTPS, 123);
+		CHECK_THROWS("www.mysite", "/doc", Protocol::HTTPS, 65536);
+		CHECK_THROWS("www.mysite", "/doc", Protocol::HTTP, 0);
+	}
+}
+
+TEST_CASE("GetUrl() must return url string representation")
+{
+	CHttpUrl url1("www.mysite", "doc", Protocol::HTTPS, 123);
+	CHECK(url1.GetURL() == "https://www.mysite:123/doc");
+
+	SECTION("Standard port for HTTP and HTTPS mustn't be included in string")
+	{
+		CHttpUrl url2("www.mysite", "doc", Protocol::HTTP);
+		CHECK(url1.GetURL() == "http://www.mysite/doc");
+	}
+}
+
+TEST_CASE("If port is not specified, then it must be default for HTTP and HTTPS")
+{
+	CHttpUrl url1("www.mysite", "doc", Protocol::HTTP);
+	CHttpUrl url2("www.mysite", "doc", Protocol::HTTPS);
+	CHECK(url1.GetPort() == 80);
+	CHECK(url2.GetPort() == 443);
+}
