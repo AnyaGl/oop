ccaa4bf0493679fb7a7943cbc9848c7cd89d2141
AnyaGl <gladysheva_2000@inbox.ru>
2020-05-22 15:42:44 +0300

    Разработана программа FindMaxEx и написаны тесты

---
+++ labs/lab7/FindMaxEx/FindMaxEx/main.cpp
@@ -0,0 +1,72 @@
+﻿#include "FindMaxEx.h"
+#include "Sportsman.h"
+#include <iostream>
+#include <sstream>
+
+Sportsman GetSportsman(const std::string& str)
+{
+	std::istringstream ss(str);
+	Sportsman sportsman;
+	ss >> sportsman.name >> sportsman.height >> sportsman.weight;
+	if (!ss)
+	{
+		throw std::runtime_error("Invalid string");
+	}
+	return sportsman;
+}
+
+std::vector<Sportsman> ReadSportsmen(std::istream& input)
+{
+	std::vector<Sportsman> sportsmen;
+	std::string inputStr;
+	while (getline(input, inputStr))
+	{
+		try
+		{
+			sportsmen.push_back(GetSportsman(inputStr));
+		}
+		catch (const std::runtime_error& exc)
+		{
+			std::cout << exc.what() << std::endl;
+		}
+	}
+	return sportsmen;
+}
+
+void PrintSportsmanWithMaxHeight(std::ostream& output, const std::vector<Sportsman>& sportsmen)
+{
+	Sportsman sportsmanWithMaxHeight;
+	if (FindMax(sportsmen, sportsmanWithMaxHeight, IsLessHeight))
+	{
+		output << "Sportsman with max height: " << sportsmanWithMaxHeight.name
+			   << "\n\theight: " << sportsmanWithMaxHeight.height
+			   << "\n\tweight: " << sportsmanWithMaxHeight.weight << std::endl;
+	}
+	else
+	{
+		output << "Cannot find sportsman with max height" << std::endl;
+	}
+}
+
+void PrintSportsmanWithMaxWeight(std::ostream& output, const std::vector<Sportsman>& sportsmen)
+{
+	Sportsman sportsmanWithMaxWeight;
+	if (FindMax(sportsmen, sportsmanWithMaxWeight, IsLessWeight))
+	{
+		output << "Sportsman with max weight: " << sportsmanWithMaxWeight.name
+			   << "\n\theight: " << sportsmanWithMaxWeight.height
+			   << "\n\tweight: " << sportsmanWithMaxWeight.weight << std::endl;
+	}
+	else
+	{
+		output << "Cannot find sportsman with max weight" << std::endl;
+	}
+}
+
+int main()
+{
+	std::vector<Sportsman> sportsmen = ReadSportsmen(std::cin);
+	PrintSportsmanWithMaxHeight(std::cout, sportsmen);
+	PrintSportsmanWithMaxWeight(std::cout, sportsmen);
+	return 0;
+}
---
+++ labs/lab7/FindMaxEx/FindMaxExTests/FindMaxExTests.cpp
@@ -0,0 +1,51 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../FindMaxEx/FindMaxEx.h"
+#include "../FindMaxEx/Sportsman.h"
+#include "catch2/catch.hpp"
+
+TEST_CASE("FindMax() must find the maximum value of vector<double>")
+{
+	std::vector<double> intVector = { 9.8, 14.12, 5.6, 44.7, 23, 31.3 };
+	double maxValue;
+	CHECK(FindMax(intVector, maxValue, std::less<double>()));
+	CHECK(maxValue == 44.7);
+}
+
+TEST_CASE("FindMax() must find the maximum value of vector<char>")
+{
+	std::vector<char> strVector = { 'a', 'b', 'z', 'y' };
+	char maxValue;
+	CHECK(FindMax(strVector, maxValue, std::less<char>()));
+	CHECK(maxValue == 'z');
+}
+
+TEST_CASE("If vector<T> is empty, FindMax() must return false")
+{
+	std::vector<int> strVector;
+	int maxValue;
+	CHECK_FALSE(FindMax(strVector, maxValue, std::less<int>()));
+}
+
+std::vector<Sportsman> sportsmen = {
+	{ "Vladimir", 185.6, 73.5 },
+	{ "Andrey", 190, 85 },
+	{ "Ivan", 183.4, 90 }
+};
+
+TEST_CASE("FindMax() must find sporstman with max height in vector<Sportsman>")
+{
+	Sportsman sportsman;
+	CHECK(FindMax(sportsmen, sportsman, IsLessHeight));
+	CHECK(sportsman.name == "Andrey");
+	CHECK(sportsman.height == 190);
+	CHECK(sportsman.weight == 85);
+}
+
+TEST_CASE("FindMax() must find sporstman with max weight in vector<Sportsman>")
+{
+	Sportsman sportsman;
+	CHECK(FindMax(sportsmen, sportsman, IsLessWeight));
+	CHECK(sportsman.name == "Ivan");
+	CHECK(sportsman.height == 183.4);
+	CHECK(sportsman.weight == 90);
+}
